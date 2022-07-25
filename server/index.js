427184cc6df2166bdd28fee634bdfdd50e9f5bc1
Yura <yuraborisov19@gmail.com>
2022-07-25 11:39:21 +0300

    practice with websokets

--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const app = express()
 const wsServer = require("express-ws")(app);
+const aWss = wsServer.getWss();
 
 const PORT = 5000;
+
+const broadcastHandler = (ws, msg) => {
+  aWss.clients.forEach((client) => {
+    if (client.id === msg.id) {
+      client.send(`Користувач з ${msg.userName} підключився`)
+    }
+  })
+}
+
+const connectionHandler = (ws, msg) => {
+  ws.id = msg.id;
+  broadcastHandler(ws, msg);
+}
+
+app.ws("/", (ws, req) => {
+  ws.on("message", (msg) => {
+    const userObject = JSON.parse(msg);
+    switch (userObject.method) {
+      case "connection": connectionHandler(ws, userObject);
+        break;
+    }
+  })
+})
+
 
 app.listen(PORT, () => {
   console.log("Server work on 5000 port");
