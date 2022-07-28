67b988ce3c86d29bf4055e0c5a02d1fda33119d6
YuraStork <80415664+YuraStork@users.noreply.github.com>
2022-07-28 11:38:03 +0300

    fix conflict
    * created canvasContext
    
    * tools state
    
    * update tools state
    
    * create pen
    
    * created pen and square and updated context
    
    * circle tool
    
    * eraser tool
    
    * line tool
    
    * updated square
    
    * updated tools
    
    * created sever
    
    * created save snapshot
    
    * created routes
    
    * updated server
    
    * created home page
    
    * updated
    
    * added cookie
    
    * added cookies on front
    
    * added cookies on front
    
    * check user in room

--- client/src/pages/enterRoom/index.tsx
+++ client/src/pages/enterRoom/index.tsx
@@ -1,4 +1,3 @@
-
 import { FC, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { checkRoom } from "../../api/rooms/checkRoom";
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const roomRouter = require("./room.router");
-
 const userRouter = require("./user.router");
 const router = Router();
 
