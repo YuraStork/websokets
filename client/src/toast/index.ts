e4e1a9350a8960e2331560fc38f4ace978b21ad3
Yura <yuraborisov19@gmail.com>
2022-07-28 15:09:49 +0300

    added communication functions

---
+++ client/src/toast/index.ts
@@ -0,0 +1,5 @@
+import { toast } from "react-toastify";
+
+export const toastError = (str: string) => toast.error(str);
+export const toastSuccess = (str: string) => toast.success(str);
+export const toastWarn = (str: string) => toast.warn(str);
